9ddfbc46d913193e4760c57308c2dce2c3a9aaa2
yingshaoxo <yingshaoxo@gmail.com>
2023-06-07 11:55:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Atlantis Space (Fake)
+
+Atlantis Space is an isolated space that could help you backup packages.
---
+++ android/app/src/main/kotlin/xyz/yingshaoxo/atlantisspace/MainActivity.kt
@@ -0,0 +1,6 @@
+package xyz.yingshaoxo.atlantisspace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
