a00fd8f0d9cc11bda92395753a1e1ec8da8ad09e
yingshaoxo <yingshaoxo@gmail.com>
2023-06-09 09:04:32 +0800

    add signing script

--- Build.sh
+++ Build.sh
@@ -1,2 +1,3 @@
 mkdir -p /tmp/atlantis_flutter
-flutter build apk --obfuscate --split-debug-info=/tmp/atlantis_space_flutter
+flutter build apk --release --obfuscate --split-debug-info=/tmp/atlantis_space_flutter
+flutter build appbundle --release --obfuscate --split-debug-info=/tmp/atlantis_space_flutter
